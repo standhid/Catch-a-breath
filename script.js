f202ce84e31fc86b82836aec8b4c9c4b70ef14af
standhid <78404519+standhid@users.noreply.github.com>
2021-07-16 03:26:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const totalTime=7500;
 const breatheTime = (totalTime/5)*2;
 const holdTime=totalTime/5;
 
-
+breatheAnimation();
 function breatheAnimation(){
     text.innerHTML='Breathe In!'
     container.className='container grow'
